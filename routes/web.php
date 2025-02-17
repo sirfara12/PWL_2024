697f6aa95edc97fc6b4b00d2af3e2cc3026a225f
sirfara <aratihsirf@gmail.com>
2025-02-17 13:24:07 +0700

    Jobsheet2/Minggu ke2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,44 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function() {
+    return 'World';
+});
+
+Route::get('/', function() {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function() {
+    return 'NIM : 2341720072 - Nama: Sirfaratih';
+});
+
+Route::get('/user/{name}', function($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) {
+ return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/Id/{id}', function ($Id) {
+ return 'Halaman Artikel dengan Id: '.$Id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/profile', function() {
+})->name('profile');
+
